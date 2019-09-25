a8984794933412ee5ec2bad7762ab0a70be80115
Hernâni <hernaniborgesdefreitas@gmail.com>
2019-09-25 16:36:47 +0100

    OPENEUROPA-2293: Fix entity routes/menus.

--- src/Form/LinkListLinkForm.php
+++ src/Form/LinkListLinkForm.php
@@ -110,7 +110,7 @@ class LinkListLinkForm extends ContentEntityForm {
           '%label' => $entity->label(),
         ]));
     }
-    $form_state->setRedirect('entity.link_list_link.canonical', ['link_list_link' => $entity->id()]);
+    $form_state->setRedirect('entity.link_list_link.edit_form', ['link_list_link' => $entity->id()]);
   }
 
 }
