b0e26a9fbe0d0bf65de39eea1df8e6337b8cfc2d
Francesco Sardara <francesco.sardara@ausy.be>
2019-11-12 13:51:18 +0100

    OPENEUROPA-2450: Allow tests to change when the bar plugin is applicable, so that method parameters are covered by tests.

--- modules/oe_link_lists_internal_source/tests/modules/oe_link_lists_internal_source_test/src/Plugin/InternalLinkSourceFilter/Bar.php
+++ modules/oe_link_lists_internal_source/tests/modules/oe_link_lists_internal_source_test/src/Plugin/InternalLinkSourceFilter/Bar.php
@@ -1,10 +1,15 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace Drupal\oe_link_lists_internal_source_test\Plugin\InternalLinkSourceFilter;
 
 use Drupal\Core\Entity\Query\QueryInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\State\StateInterface;
 use Drupal\oe_link_lists_internal_source\InternalLinkSourceFilterPluginBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Test implementation of an internal link source filter.
@@ -15,7 +20,44 @@ use Drupal\oe_link_lists_internal_source\InternalLinkSourceFilterPluginBase;
  *   description = @Translation("Bar description."),
  * )
  */
-class Bar extends InternalLinkSourceFilterPluginBase {
+class Bar extends InternalLinkSourceFilterPluginBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The state service.
+   *
+   * @var \Drupal\Core\State\StateInterface
+   */
+  protected $state;
+
+  /**
+   * Bar constructor.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\State\StateInterface $state
+   *   The state service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, StateInterface $state) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->state = $state;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('state')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -50,6 +92,16 @@ class Bar extends InternalLinkSourceFilterPluginBase {
    */
   public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
     $this->configuration['include'] = $form_state->getValue('include');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isApplicable(string $entity_type, string $bundle): bool {
+    $allowed_entity_types = $this->state->get('internal_source_test_bar_entity_types', []);
+    $allowed_bundles = $this->state->get('internal_source_test_bar_bundles', []);
+
+    return in_array($entity_type, $allowed_entity_types) && in_array($bundle, $allowed_bundles);
   }
 
   /**
