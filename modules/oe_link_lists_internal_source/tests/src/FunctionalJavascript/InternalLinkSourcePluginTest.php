074fb8b5cea38b523285587d96165e7da6d83d15
Francesco Sardara <francesco.sardara@ausy.be>
2019-11-26 15:56:58 +0100

    OPENEUROPA-2544: Make a base test class for internal link source browser tests.

--- modules/oe_link_lists_internal_source/tests/src/FunctionalJavascript/InternalLinkSourcePluginTest.php
+++ modules/oe_link_lists_internal_source/tests/src/FunctionalJavascript/InternalLinkSourcePluginTest.php
@@ -4,23 +4,18 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\oe_link_lists_internal_source\FunctionalJavascript;
 
-use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
-use Drupal\oe_link_lists\Entity\LinkListInterface;
-
 /**
  * Tests the internal link source plugin.
  *
  * @group oe_link_lists
  */
-class InternalLinkSourcePluginTest extends WebDriverTestBase {
+class InternalLinkSourcePluginTest extends InternalLinkSourceTestBase {
 
   /**
    * {@inheritdoc}
    */
   protected static $modules = [
     'node',
-    'oe_link_lists_test',
-    'oe_link_lists_internal_source',
   ];
 
   /**
@@ -159,39 +154,6 @@ class InternalLinkSourcePluginTest extends WebDriverTestBase {
       'bundle' => 'user',
       'filters' => [],
     ], $link_list->getConfiguration()['source']['plugin_configuration']);
-  }
-
-  /**
-   * Returns a link list entity given its title.
-   *
-   * @param string $title
-   *   The link list title.
-   * @param bool $reset
-   *   Whether to reset the link list entity cache. Defaults to FALSE.
-   *
-   * @return \Drupal\oe_link_lists\Entity\LinkListInterface|null
-   *   The first link list entity that matches the title. NULL if not found.
-   */
-  protected function getLinkListByTitle(string $title, bool $reset = FALSE): ?LinkListInterface {
-    $storage = \Drupal::entityTypeManager()->getStorage('link_list');
-    if ($reset) {
-      $storage->resetCache();
-    }
-
-    $entities = $storage->loadByProperties(['title' => $title]);
-
-    if (empty($entities)) {
-      return NULL;
-    }
-
-    return reset($entities);
-  }
-
-  /**
-   * Disables the native browser validation for required fields.
-   */
-  protected function disableNativeBrowserRequiredFieldValidation() {
-    $this->getSession()->executeScript("jQuery(':input[required]').prop('required', false);");
   }
 
 }
