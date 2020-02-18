e7b93f6dda99893c1227d3e5c9a63984314de9b1
Francesco Sardara <francesco.sardara@ausy.be>
2020-02-18 13:59:26 +0100

    OPENEUROPA-2783: Simplify the test.

--- modules/oe_link_lists_rss_source/modules/oe_link_lists_rss_source_access/tests/Kernel/LinkListRssAccessTest.php
+++ modules/oe_link_lists_rss_source/modules/oe_link_lists_rss_source_access/tests/Kernel/LinkListRssAccessTest.php
@@ -4,34 +4,25 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\oe_link_lists_rss_source_acces\Kernel;
 
+use Drupal\aggregator\Entity\Feed;
+use Drupal\aggregator\Entity\Item;
 use Drupal\aggregator\FeedStorageInterface;
-use Drupal\Core\Http\ClientFactory;
-use Drupal\KernelTests\KernelTestBase;
-use Drupal\Tests\user\Traits\UserCreationTrait;
-use GuzzleHttp\Client;
-use GuzzleHttp\Psr7\Response;
-use Psr\Http\Message\RequestInterface;
+use Drupal\KernelTests\Core\Entity\EntityKernelTestBase;
 
 /**
  * Tests that proper access checks are run on link list rendering.
  *
  * @group oe_link_lists
  */
-class LinkListRssAccessTest extends KernelTestBase {
+class LinkListRssAccessTest extends EntityKernelTestBase {
 
-  use UserCreationTrait;
   /**
    * {@inheritdoc}
    */
-  protected static $modules = [
-    'user',
+  public static $modules = [
     'aggregator',
     'options',
-    'system',
-    'oe_link_lists',
-    'oe_link_lists_rss_source',
     'oe_link_lists_rss_source_access',
-
   ];
 
   /**
@@ -40,10 +31,7 @@ class LinkListRssAccessTest extends KernelTestBase {
   protected function setUp() {
     parent::setUp();
 
-    $this->installSchema('system', ['sequences']);
-
     $this->installConfig('aggregator');
-    $this->installEntitySchema('user');
     $this->installEntitySchema('aggregator_feed');
     $this->installEntitySchema('aggregator_item');
 
@@ -52,75 +40,51 @@ class LinkListRssAccessTest extends KernelTestBase {
     // is run).
     $this->config('aggregator.settings')->set('items.expire', FeedStorageInterface::CLEAR_NEVER)->save();
 
-    // Mock the http client and factory to allow requests to certain RSS feeds.
-    $http_client_mock = $this->getMockBuilder(Client::class)->getMock();
-    $test_module_path = drupal_get_path('module', 'aggregator_test');
-    $http_client_mock
-      ->method('send')
-      ->willReturnCallback(function (RequestInterface $request, array $options = []) use ($test_module_path) {
-        switch ($request->getUri()) {
-          case 'http://www.example.com/atom.xml':
-            $filename = $test_module_path . DIRECTORY_SEPARATOR . 'aggregator_test_atom.xml';
-            break;
-
-          case 'http://www.example.com/rss.xml':
-            $filename = $test_module_path . DIRECTORY_SEPARATOR . 'aggregator_test_rss091.xml';
-            break;
-
-          case 'http://ec.europa.eu/rss.xml':
-            $filename = drupal_get_path('module', 'oe_link_lists_rss_source') . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR . 'fixtures' . DIRECTORY_SEPARATOR . 'rss_links_source_test_rss.xml';
-            break;
-
-          default:
-            return new Response(404);
-        }
-
-        return new Response(200, [], file_get_contents($filename));
-      });
-
-    $http_client_factory_mock = $this->getMockBuilder(ClientFactory::class)
-      ->disableOriginalConstructor()
-      ->getMock();
-    $http_client_factory_mock->method('fromOptions')
-      ->willReturn($http_client_mock);
-
-    $this->container->set('http_client_factory', $http_client_factory_mock);
-
-    $this->container->get('module_handler')->loadInclude('user', 'install');
-    $this->installEntitySchema('user');
-    user_install();
+    // Create a UID 1 user to be able to create test users with particular
+    // permissions in the tests.
+    $this->drupalCreateUser();
   }
 
   /**
    * Test access to the aggregator module routes.
    */
   public function testFeedsAccess() {
-    $user_privileged = $this->createUser(['view feed items'], 'user_privileged');
-    $user_not_privileged = $this->createUser(['access news feeds'], 'user_not_privileged');
-
-    $feed_storage = $this->container->get('entity_type.manager')->getStorage('aggregator_feed');
-    $feed = $feed_storage->create([
+    // Create an aggregator feed.
+    $aggregator_feed = Feed::create([
       'title' => $this->randomString(),
-      'url' => 'http://www.example.com/rss.xml',
+      'url' => 'http://www.example.com',
     ]);
-    $feed->save();
-    $feed->refreshItems();
+    $aggregator_feed->save();
+    // Create an item inside the feed.
+    $feed_item = Item::create([
+      'fid' => $aggregator_feed->id(),
+      'title' => $this->randomString(),
+      'path' => 'http://www.example.com/1',
+    ]);
+    $feed_item->save();
 
-    $this->assertFalse($feed->access('view', $user_privileged));
-    $this->assertTrue($feed->access('view', $user_not_privileged));
+    $access_handler = $this->container->get('entity_type.manager')->getAccessControlHandler('aggregator_item');
+    $user_without_permission = $this->drupalCreateUser();
+    $this->assertFalse($access_handler->access($feed_item, 'view', $user_without_permission));
 
-    /** @var \Drupal\aggregator\ItemStorageInterface $storage */
-    $storage = \Drupal::entityTypeManager()->getStorage('aggregator_item');
-    $query = $storage->getQuery()
-      ->condition('fid', $feed->id())
-      ->sort('timestamp', 'DESC')
-      ->sort('iid', 'DESC');
-    $ids = $query->execute();
+    $user_with_permission = $this->drupalCreateUser(['view feed items']);
+    $this->assertTrue($access_handler->access($feed_item, 'view', $user_with_permission));
 
-    $feed_items = $storage->loadMultiple($ids);
-    $feed_item = reset($feed_items);
-    $this->assertTrue($feed_item->access('view', $user_privileged));
-    $this->assertTrue($feed_item->access('view', $user_not_privileged));
+    // Verify that the default aggregator permission still works correctly.
+    $feed_access_user = $this->drupalCreateUser(['access news feeds']);
+    $this->assertTrue($access_handler->access($feed_item, 'view', $feed_access_user));
+
+    // Verify that a user with both permissions still has proper access.
+    $feed_access_user = $this->drupalCreateUser([
+      'access news feeds',
+      'view feed items',
+    ]);
+    $this->assertTrue($access_handler->access($feed_item, 'view', $feed_access_user));
+
+    // The view item permission doesn't give access to other operations.
+    $this->assertFalse($access_handler->access($feed_item, 'update', $user_with_permission));
+    $this->assertFalse($access_handler->access($feed_item, 'delete', $user_with_permission));
+    $this->assertFalse($access_handler->createAccess(NULL, $user_with_permission));
   }
 
 }
